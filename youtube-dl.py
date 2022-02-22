83829b3a765178d3a314faaf4fbf5691967f2d7d
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-02-22 10:25:40 +0900

    d

--- youtube-dl.py
+++ youtube-dl.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT") or 5000)
 
 @app.route('/')
 def index():
-   return "<h1>Test</h1>"
+   return f"<h1>Test port: {port}</h1>"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT") or 5000)
     app.run(port=port)
