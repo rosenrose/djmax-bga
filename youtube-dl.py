a7b2ee4d4b592898ae127028600026b149541beb
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-02-24 23:44:53 +0900

    get random frame

--- youtube-dl.py
+++ youtube-dl.py
@@ -1,11 +1,34 @@
 import os
 import re
 import json
+import random
+import ffmpeg
 from yt_dlp import YoutubeDL
-from flask import Flask, Response
+from flask import Flask, Response, make_response
 
 app = Flask(__name__)
 port = int(os.environ.get("PORT") or 5000)
+
+def get_info(id):
+    result = {}
+
+    if re.match(r"[0-9a-zA-Z_\-]{11}", id):
+        with YoutubeDL({"simulate": True}) as ydl:
+            result = ydl.extract_info(f"https://youtu.be/{id}")
+
+    return result
+
+def get_url(info):
+    src = {"video": None, "audio": None}
+
+    if "requested_formats" in info:
+        for format in info["requested_formats"]:
+            if format["vcodec"] != "none":
+                src["video"] = format["url"]
+            elif format["acodec"] != "none":
+                src["audio"] = format["url"]
+
+    return src
 
 @app.route('/')
 def index():
@@ -13,23 +36,34 @@ def index():
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
-@app.route('/<id>')
-def get_url(id):
-    if not re.match(r"[0-9a-zA-Z_\-]{11}", id):
-        response = Response("")
+@app.route('/url/<id>')
+def url(id):
+    response = Response(json.dumps(get_url(get_info(id))))
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+@app.route('/frame/<id>')
+def frame(id):
+    info = get_info(id)
+    duration = info["duration"]
+    videoUrl = get_url(info)["video"]
+
+    if not videoUrl:
+        response = Response("no video")
         response.headers["Access-Control-Allow-Origin"] = "*"
         return response
 
-    src = {}
-    with YoutubeDL({"simulate": True}) as ydl:
-        info = ydl.extract_info(f"https://youtu.be/{id}")
-        for format in info["requested_formats"]:
-            if format["vcodec"] != "none":
-                src["video"] = format["url"]
-            elif format["acodec"] != "none":
-                src["audio"] = format["url"]
-    response = Response(json.dumps(src))
+    out, err = (
+        ffmpeg
+        .input(videoUrl, ss=random.randrange(duration))
+        .output("pipe:", format="image2", vcodec="png", frames=1, s="1920x1080")
+        .run(capture_stdout=True)
+    )
+    # print(type(out))
+    open("a.png", "wb").write(out)
+    response = make_response(out)
     response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Content-Type"] = "image/png"
     return response
 
 if __name__ == "__main__":
